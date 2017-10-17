a27710423747aee177f3d881aab903c84d2b46e4
cgx <cgx>
2017-10-17 14:40:04 +0800

    添加Launcher3-O项目

---
+++ src/com/android/launcher3/util/Const.java
@@ -0,0 +1,12 @@
+package com.android.launcher3.util;
+
+import com.android.launcher3.BuildConfig;
+
+/**
+ * Created by cgx on 2017/10/17.
+ *
+ */
+
+public class Const {
+    public static final boolean DEBUG = BuildConfig.LOG_DEBUG;
+}
